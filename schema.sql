63aaeb3dee53c8299b04b3ca339357226f1cbce8
Conor McCue <conormccue@gmail.com>
2019-11-07 09:55:03 -0800

    reformat schema

--- schema.sql
+++ schema.sql
@@ -1,4 +1,6 @@
-CREATE TABLE IF NOT EXISTS city_explorer (
+DROP TABLE IF EXISTS city_explorer; 
+
+CREATE TABLE city_explorer(
   id SERIAL PRIMARY KEY,
   latitude NUMERIC (20, 8),
   longitude NUMERIC (20, 8)
