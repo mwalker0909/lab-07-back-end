bf657c9a7cadedd2ed73aed40718801b4349a8e3
Conor McCue <conormccue@gmail.com>
2019-11-08 00:21:34 -0800

    reconfigure schema

--- schema.sql
+++ schema.sql
@@ -2,7 +2,8 @@ DROP TABLE IF EXISTS locations;
 
 CREATE TABLE locations(
   id SERIAL PRIMARY KEY,
-  city_name TEXT,
+  search_query TEXT,
+  formatted_query TEXT,
   latitude NUMERIC (20, 8),
   longitude NUMERIC (20, 8)
 );
--- server.js
+++ server.js
@@ -18,69 +18,13 @@ app.use(cors());
 
 
 
-//Define Port to be listened to
+//Establish Db
 const client = new pg.Client(process.env.DATABASE_URL);
 client.on('error', err => console.error(err));
 
-
-
-
-
 //Define Functional Routes
-//Requests to /location are handled with the handleLocation function.
+//Requests to /location are handled with the getLocation function.
 app.get('/location', getLocation);
-
-//Requests to /weather are handled with the handleWeather function.
-app.get('/weather', getWeather);
-
-
-//Requests to root are handled with the landingPage function.
-app.get('/', landingPage);
-
-
-// Helper Functions
-function landingPage(request, response) {
-  response.status(200).send('Welcome to my back-end.');
-}
-
-Location.prototype.save = function(){
-  const SQL = `INSERT INTO locations(search_query, formatted_query, latitude, longitude) VALUES('$1', '$2', $3, $4) RETURNING *`;
-  
-}
-
-Location.fetchLocation = function(){
-  //fetch API
-}
-
-function getLocation(request, response){
-
-  const locationHandler = {
-    query: request.query.data,
-
-    cacheHit: results => response.send(results.rows[0]),
-
-    cacheMiss: () => {
-      Location.fetchLocation(request.query.data)
-        .then( data=> response.send(data));
-    }
-  };
-  Location.lookup(locationHandler);
-}
-
-function handleWeather(request, response){
-  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
-  superagent.get(url)
-    .then( data => {
-      const weatherData = data.body;
-      const dailyWeather = weatherData.daily.data;
-      let newdailyWeather = dailyWeather.map(day => new Forecast(day));
-      response.status(200).send(newdailyWeather);
-    })
-    .catch( error => {
-      console.error(error);
-      response.status(500).send('Status: 500. Sorry, there is something not quite right');
-    })
-}
 
 function Location(city, geoData){
   this.search_query = city;
@@ -88,14 +32,6 @@ function Location(city, geoData){
   this.latitude = geoData.results[0].geometry.location.lat;
   this.longitude = geoData.results[0].geometry.location.lng;
 }
-
-// TODO: Define prototype for Location Constructor that saves data
-Location.prototype.save = function(){
-  const SQL = `INSERT INTO locations(search_query, formatted_query, latitude, longitude) VALUES ($1,$2,$3,$4) RETURNING *`;
-
-  let values = Object.values(this);
-  return client.query(SQL, values);
-};
 
 Location.fetchLocation = function (query){
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
@@ -105,12 +41,20 @@ Location.fetchLocation = function (query){
       if (result.body.results.length) {throw 'No data';}
       let location = new Location(query, result.body.results[0]);
       return location.save()
-      .then( result => {
-        location.id = result.rows[0].id;
-        return location;
-      });
+        .then( result => {
+          location.id = result.rows[0].id;
+          return location;
+        });
     });
 };
+
+Location.prototype.save = function(){
+  const SQL = `INSERT INTO locations(search_query, formatted_query, latitude, longitude) VALUES ($1,$2,$3,$4) RETURNING *`;
+
+  let values = Object.values(this);
+  return client.query(SQL, values);
+};
+
 
 Location.lookup = (handler) => {
   const SQL = `SELECT * FROM locations WHERE search_query=$1`;
@@ -127,20 +71,63 @@ Location.lookup = (handler) => {
     .catch(console.error);
 };
 
+function getLocation(request, response){
+
+  const locationHandler = {
+    query: request.query.data,
+
+    cacheHit: results => response.send(results.rows[0]),
+
+    cacheMiss: () => {
+      Location.fetchLocation(request.query.data)
+        .then( data=> response.send(data));
+    }
+  };
+  Location.lookup(locationHandler);
+}
+
+
+
+
+
+
+
+//Requests to /weather are handled with the handleWeather function.
+app.get('/weather', getWeather);
+
+
+//Requests to root are handled with the landingPage function.
+app.get('/', landingPage);
+
+
+// Helper Functions
+function landingPage(request, response) {
+  response.status(200).send('Welcome to my back-end.');
+}
+
+
+
+function getWeather(request, response){
+  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
+  superagent.get(url)
+    .then( data => {
+      const weatherData = data.body;
+      const dailyWeather = weatherData.daily.data;
+      let newdailyWeather = dailyWeather.map(day => new Forecast(day));
+      response.status(200).send(newdailyWeather);
+    })
+    .catch( error => {
+      console.error(error);
+      response.status(500).send('Status: 500. Sorry, there is something not quite right');
+    })
+}
+
+
+
 
 function Forecast (day){
   this.forecast = day.summary;
   this.time = new Date(day.time * 1000).toDateString();
-}
-
-function checkDB (input, request, response){
-  const SQL = `SELECT * FROM locations WHERE city_name LIKE ('${input}')`;
-  client.query(SQL, (error, resolve) => {
-    error ? console.error(error) : 
-    resolve.rowCount === 0 ? handleLocation(request, response):
-    resolve.rows[0].
-    //TODO: Take the thing out of database, format, return;
-  });
 }
 
 //Non-valid page response
